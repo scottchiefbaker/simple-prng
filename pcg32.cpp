a7d04a94485383592d0923c297fac743281905e9
Scott Baker <scott@perturb.org>
2024-10-22 11:58:33 -0700

    Add the first three implementations

---
+++ pcg32.cpp
@@ -0,0 +1,62 @@
+/////////////////////////////////////////////////////////////////////////
+// PCG32 wrapper written by Scott Baker / https://www.perturb.org      //
+// Written: 2024-10-24                                                 //
+/////////////////////////////////////////////////////////////////////////
+
+/* This is a fixed-increment version of Java 8's SplittableRandom generator
+   See http://dx.doi.org/10.1145/2714064.2660195 and
+   http://docs.oracle.com/javase/8/docs/api/java/util/SplittableRandom.html
+
+   It is a very fast generator passing BigCrush, and it can be useful if
+   for some reason you absolutely want 64 bits of state. */
+
+#include <cstdint>
+
+class pcg32 {
+	public:
+		void seed(uint64_t seed1, uint64_t seed2);
+		void seed(uint64_t seeds[2]);
+		uint32_t rand();
+		uint64_t rand64();
+};
+
+//////////////////////////////////////////////////////////////
+
+static uint64_t state;
+static uint64_t inc;
+
+void pcg32::seed(uint64_t seed1, uint64_t seed2) {
+	state = seed1;
+	inc   = seed2;
+
+	//printf("%llu / %llu\n", state, inc);
+}
+
+void pcg32::seed(uint64_t seeds[2]) {
+	state = seeds[0];
+	inc   = seeds[1];
+
+	//printf("%llu / %llu\n", state, inc);
+}
+
+uint32_t pcg32::rand() {
+    uint64_t oldstate = state;
+
+    // Advance internal state
+    state = oldstate * 6364136223846793005ULL + (inc|1);
+
+    // Calculate output function (XSH RR), uses old state for max ILP
+    uint32_t xorshifted = ((oldstate >> 18u) ^ oldstate) >> 27u;
+    uint32_t rot = oldstate >> 59u;
+
+    return (xorshifted >> rot) | (xorshifted << ((-rot) & 31));
+}
+
+uint64_t pcg32::rand64() {
+	uint32_t low  = pcg32::rand();
+	uint64_t high = pcg32::rand();
+
+	uint64_t ret = (high << 32) | low;
+
+	return ret;
+}
---
+++ split_mix_64.cpp
@@ -0,0 +1,53 @@
+/////////////////////////////////////////////////////////////////////////
+// SplitMix64 wrapper written by Scott Baker / https://www.perturb.org //
+// Written: 2024-10-24                                                 //
+/////////////////////////////////////////////////////////////////////////
+
+/* This is a fixed-increment version of Java 8's SplittableRandom generator
+   See http://dx.doi.org/10.1145/2714064.2660195 and
+   http://docs.oracle.com/javase/8/docs/api/java/util/SplittableRandom.html
+
+   It is a very fast generator passing BigCrush, and it can be useful if
+   for some reason you absolutely want 64 bits of state. */
+
+#include <cstdint>
+
+class sm64 {
+	public:
+		void seed(uint64_t seed1, uint64_t seed2);
+		void seed(uint64_t seeds[2]);
+		uint32_t rand();
+		uint64_t rand64();
+};
+
+//////////////////////////////////////////////////////////////
+
+static uint64_t x; /* The state can be seeded with any value. */
+
+// There are two seeds here to be compatible with other PRNGs
+// The second is ignore for split mix
+void sm64::seed(uint64_t seed1, uint64_t seed2) {
+	x = seed1;
+}
+
+void sm64::seed(uint64_t seeds[1]) {
+	x = seeds[0];
+}
+
+uint64_t sm64::rand64() {
+	uint64_t z;
+
+	z = (x += 0x9e3779b97f4a7c15);
+	z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
+	z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
+
+	return z ^ (z >> 31);
+}
+
+uint32_t sm64::rand() {
+	uint64_t ret = sm64::rand64();
+
+	ret = ret >> 32;
+
+	return ret;
+}
