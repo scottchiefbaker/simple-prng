9f9a2e0da2db7f62cede250155e50324f42370ec
Scott Baker <scott@perturb.org>
2025-01-26 16:47:45 -0800

    Rearrange a bunch of functions

--- prng-test.cpp
+++ prng-test.cpp
@@ -8,11 +8,6 @@
 #include "xoshiro256starstar.cpp"
 #include "skel.cpp"
 
-uint64_t get_urandom_u64();
-uint64_t nanos();
-
-uint8_t iterations = 4;
-
 // Pick which PRNG you want to use
 pcg32 prng;
 
@@ -20,64 +15,6 @@ pcg32 prng;
 //xoroshiro128plus prng;
 //xoshiro256starstar prng;
 //skel prng;
-
-int main(int argc, char *argv[]) {
-	// Get a random number from /dev/urandom or fall back to nanos()
-	uint64_t seed = get_urandom_u64();
-	if (seed == 0) { seed = nanos(); }
-
-	// We make a small splitmix64 PRNG to generate seeds
-	splitmix64 sm;
-	sm.seed(seed);
-	sm.warmup();
-
-	// Enable debug so we print out seeds to sanity check them
-	prng.debug = 1;
-
-	// Use SplitMix64 to generate some seeds
-	uint64_t seeds[4];
-	for (int i = 0; i < 4; i++) {
-		seeds[i] = sm.rand64();
-	}
-
-	// Seed the PRNG we're wanting numbers from
-	prng.seed(seeds);
-
-	// Warm up the PRNG
-	for (int i = 0; i < 1024; i++) {
-		prng.rand();
-	}
-
-	printf("\n");
-	// 32 bit randoms
-	for (int i = 0; i < iterations; i++) {
-		printf("rand32(): %llu\n", prng.rand());
-	}
-
-	printf("\n");
-	// 64 bit randoms
-	for (int i = 0; i < iterations; i++) {
-		printf("rand64(): %llu\n", prng.rand64());
-	}
-
-	printf("\n");
-	double max = 18446744073709551615.0; // 2^64 - 1
-	for (int i = 0; i < iterations; i++) {
-		double num = prng.rand64() / max;
-		// A double is about 16 decimal digits
-		printf("float   : %0.16f\n", num);
-	}
-
-	printf("\n");
-	// Biased random number in a range
-	for (int i = 0; i < iterations; i++) {
-		uint64_t num   = prng.rand64();
-		uint64_t range = 999;
-
-		// A double is about 16 decimal digits
-		printf("0 - %-4d: %d\n", range, num % range);
-	}
-}
 
 uint64_t get_urandom_u64() {
 	uint64_t random_value = 0; // Default to 0 in case of failure
@@ -114,6 +51,60 @@ uint64_t nanos() {
 	uint64_t ret = (uint64_t)ts.tv_sec * 1000000000ULL + (uint64_t)ts.tv_nsec;
 
 	return ret;
+}
+
+// https://prng.di.unimi.it/#remarks
+static double _uint64_to_double(uint64_t num) {
+	// A standard 64bit double floating-point number in IEEE floating point
+	// format has 52 bits of significand. Thus, the representation can actually
+	// store numbers with 53 significant binary digits.
+	double scale = 1.0 / (1ULL << 53);  // 1 divided by 2^53
+	double ret   = (num >> 11) * scale; // Top 53 bits divided by 1/2^53
+
+	//printf("Double: %0.15f\n", ret);
+
+	return ret;
+}
+
+int main(int argc, char *argv[]) {
+	uint8_t iterations = 5;
+
+	prng.seed(get_urandom_u64(), get_urandom_u64());
+
+	// Warm up the PRNG
+	for (int i = 0; i < 1024; i++) {
+		prng.rand();
+	}
+
+	printf("\n");
+	// 32 bit randoms
+	for (int i = 0; i < iterations; i++) {
+		printf("rand32(): %llu\n", prng.rand());
+	}
+
+	printf("\n");
+	// 64 bit randoms
+	for (int i = 0; i < iterations; i++) {
+		printf("rand64(): %lu\n", prng.rand64());
+	}
+
+	printf("\n");
+	double max = 18446744073709551615.0; // 2^64 - 1
+	for (int i = 0; i < iterations; i++) {
+		double num = prng.rand64() / max;
+		// A double is about 16 decimal digits
+		printf("float   : %0.16f\n", num);
+	}
+
+	printf("\n");
+	// Biased random number in a range
+	for (int i = 0; i < iterations; i++) {
+		uint64_t num   = prng.rand64();
+		uint64_t range = 999;
+
+		// A double is about 16 decimal digits
+		printf("0 - %-4lu: %lu\n", range, num % range);
+	}
 }
 
 // vim: tabstop=4 shiftwidth=4 noexpandtab autoindent softtabstop=4
